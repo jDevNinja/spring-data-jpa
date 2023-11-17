ff67356ae09424d3cc6c6d1b9bd3b5d58386691c
Oleg Primolennyy <Oleg.Primolennyy@x5.ru>
2023-11-18 01:34:33 +0700

    4. commit

--- src/main/java/org/onex/element/JpaConfig.java
+++ src/main/java/org/onex/element/JpaConfig.java
@@ -1,27 +1,48 @@
 package org.onex.element;
 
+import java.util.Properties;
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 @Configuration
-
+@EnableJpaRepositories(basePackages = "org.onex.element")
 public class JpaConfig {
+  @Bean
+  public DataSource dataSource() {
+    DriverManagerDataSource dataSource =
+        new DriverManagerDataSource("jdbc:postgresql://localhost:5442/postgres", "user", "pass");
+    dataSource.setDriverClassName("org.postgresql.Driver");
+    return dataSource;
+  }
 
-	@Bean
-	public LocalEntityManagerFactoryBean entityManagerFactory() {
-		LocalEntityManagerFactoryBean emf = new LocalEntityManagerFactoryBean();
-		emf.setPersistenceUnitName("any-name-is-ok");
-		return emf;
-	}
+  @Bean
+  public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
+    LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+    emf.setDataSource(dataSource);
+    emf.setPackagesToScan("org.onex.element");
+    emf.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
 
-	@Bean
-	public DataSource dataSource() {
-		DriverManagerDataSource dataSource =
-			new DriverManagerDataSource("jdbc:postgresql://localhost:5442/postgres", "user", "pass");
-		dataSource.setDriverClassName("org.postgresql.Driver");
-		return dataSource;
-	}
+    Properties jpaProperties = new Properties();
+    jpaProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+    jpaProperties.setProperty("hibernate.show_sql", "true"); // Показывать SQL запросы
+    jpaProperties.setProperty("hibernate.hbm2ddl.auto", "update");
+
+    emf.setJpaProperties(jpaProperties);
+    return emf;
+  }
+
+  @Bean
+  public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+    JpaTransactionManager transactionManager = new JpaTransactionManager();
+    transactionManager.setEntityManagerFactory(entityManagerFactory);
+    return transactionManager;
+  }
 }
--- src/main/java/org/onex/element/Main.java
+++ src/main/java/org/onex/element/Main.java
@@ -4,7 +4,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
-	public static void main(String[] args) {
-		ApplicationContext context = new AnnotationConfigApplicationContext(JpaConfig.class);
-	}
+  public static void main(String[] args) {
+    ApplicationContext context = new AnnotationConfigApplicationContext(JpaConfig.class);
+    System.out.println();
+  }
 }
